cf0d1b03ea8206dd15b7f33cde2fee663d6a60d9
Peter Wilson <git-commit-by@peterwilson.cc>
2015-06-08 16:57:12 +1000

    Basic JS header

---
+++ source/assets/js/functions.js
@@ -0,0 +1,13 @@
+window.JOY = window.JOY || {};
+(function( window, undefined ){
+	// avoid scope look ups
+	var document = window.document,
+		JOY = window.JOY,
+		config = JOY.config;
+
+	if ( false === JOY.mustard ) {
+		// Browser failed cuts mustard check in header
+		return;
+	}
+
+}( window ));
