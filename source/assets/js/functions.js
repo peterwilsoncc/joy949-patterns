aed3f98b57c2133ca533709cf07fea572450138d
Peter Wilson <git-commit-by@peterwilson.cc>
2015-08-09 13:47:30 +1000

    Basic JavaScript
    - production ready console.log
    - querySelector(|All) shortcuts

--- source/assets/js/functions.js
+++ source/assets/js/functions.js
@@ -1,15 +1,68 @@
+/* global JOY949PL */
+window.console = window.console || {
+	log: function(){}
+};
 window.JOY949 = window.JOY949 || {};
 (function( window, undefined ){
 	// avoid scope look ups
 	var document = window.document,
 		JOY949 = window.JOY949,
-		config = JOY949.config;
+		config = JOY949.config,
+		console = {};
 
 	if ( false === JOY949.mustard ) {
 		// Browser failed cuts mustard check in header
 		return;
 	}
 
+	/* build up a console that only runs when using the pattern library */
+	function myConsole( method ) {
+		if ( typeof window.console[method] === 'function' ) {
+			console[method] = function(){
+				if ( JOY949PL && ( true === JOY949PL.debug ) ) {
+					window.console[method].apply( window.console, arguments );
+				}
+			};
+		}
+		else {
+			console[method] = window.console[method];
+		}
+	}
 
-	console.log( JOY949 );
+	for( var method in window.console ) {
+		myConsole( method );
+	}
+	/* end building of console */
+
+	function qsaContext( context ) {
+		if ( undefined === context ) {
+			// the context was not passed, default to document
+			context = document;
+		}
+		else if ( !context.childNodes ) {
+			return null;
+		}
+		return context;
+	}
+
+	// shortcut for querySelector
+	function $(selector, context) {
+		context = qsaContext( context );
+		if ( null === context ) {
+			return null;
+		}
+		return context.querySelector( selector );
+	}
+	
+	// shortcut for querySelectorAll
+	function $$(selector, context) {
+		context = qsaContext( context );
+		if ( null === context ) {
+			return null;
+		}
+		return context.querySelectorAll( selector );
+	}
+
+	// these are to pass early jshint :D
+	console.log( config, $$('body'), $('body'));
 }( window ));
