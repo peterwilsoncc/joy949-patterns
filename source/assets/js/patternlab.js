peterwilsoncc/joy949-patterns

commit de6417030e3badfef8173f6085707ec1a3e6b55f

gh-5 add color swatches
Color swatches for the Joy 949 site.

Resolves gh-5.

source/assets/js/patternlab.js
@@ -8,6 +8,7 @@
 	
 	function init(){
 		fillFonts();
+		swatches();
 	}
 
 	function waitingFor$(){
@@ -40,6 +41,33 @@
 		};
 		
 		wait( test, callback );
+	}
+
+	function swatches(){
+		var $colors = $( '.sg-colors' );
+		var $swatches = $colors.find( '.sg-swatch' );
+
+		$swatches.each( showHexCode );
+		
+		function showHexCode() {
+			var $swatch = $( this );
+			var $value  = $swatch.next();
+			var color   = $swatch.css( 'backgroundColor' );
+			
+			$value.text( rgb2hex( color ).toUpperCase() );
+		}
+		
+		function rgb2hex(rgb) {
+			if (/^#[0-9A-F]{6}$/i.test(rgb)) {
+				return rgb;
+			}
+			
+			rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+			function hex(x) {
+				return ("0" + parseInt(x).toString(16)).slice(-2);
+			}
+			return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+		}
 	}
 	
 	function wait( test, callback, tryFor, tryEvery ) {

source/_patterns/00-atoms/01-global/00-colors.mustache
@@ -1,38 +1,42 @@
 <ul class="sg-colors">
 	<li>
-		<span class="sg-swatch" style="background: #ff0000;"></span>
-		<span class="sg-label">#ff0000</span>
+		<span class="sg-swatch sg-swatch--pink"></span>
+		<span class="sg-label"></span>
 	</li>
 	<li>
-		<span class="sg-swatch" style="background: #00ff00;"></span>
-		<span class="sg-label">#00ff00</span>
+		<span class="sg-swatch sg-swatch--pink-light"></span>
+		<span class="sg-label"></span>
 	</li>
 	<li>
-		<span class="sg-swatch" style="background: #0000ff;"></span>
-		<span class="sg-label">#0000ff</span>
+		<span class="sg-swatch sg-swatch--blue"></span>
+		<span class="sg-label"></span>
 	</li>
 	<li>
-		<span class="sg-swatch" style="background: #ffff00;"></span>
-		<span class="sg-label">#ffff00</span>
+		<span class="sg-swatch sg-swatch--green"></span>
+		<span class="sg-label"></span>
 	</li>
 	<li>
-		<span class="sg-swatch" style="background: #00ffff;"></span>
-		<span class="sg-label">#00ffff</span>
+		<span class="sg-swatch sg-swatch--black"></span>
+		<span class="sg-label"></span>
 	</li>
 	<li>
-		<span class="sg-swatch" style="background: #ff00ff;"></span>
-		<span class="sg-label">#ff00ff</span>
+		<span class="sg-swatch sg-swatch--white"></span>
+		<span class="sg-label"></span>
 	</li>
 	<li>
-		<span class="sg-swatch" style="background: #ffffff;"></span>
-		<span class="sg-label">#ffffff</span>
+		<span class="sg-swatch sg-swatch--off-white"></span>
+		<span class="sg-label"></span>
 	</li>
 	<li>
-		<span class="sg-swatch" style="background: #808080;"></span>
-		<span class="sg-label">#808080</span>
+		<span class="sg-swatch sg-swatch--grey3"></span>
+		<span class="sg-label"></span>
 	</li>
 	<li>
-		<span class="sg-swatch" style="background: #000000;"></span>
-		<span class="sg-label">#000000</span>
+		<span class="sg-swatch sg-swatch--grey7"></span>
+		<span class="sg-label"></span>
+	</li>
+	<li>
+		<span class="sg-swatch sg-swatch--greya"></span>
+		<span class="sg-label"></span>
 	</li>
 </ul>

source/assets/css/patternlab.scss
@@ -1,4 +1,56 @@
+// ************************************
+// Sass config
+// ************************************
+
+@import "scss/10-config/defaults";  // variables with !default settings
+@import "scss/10-config/variables"; // variables
+@import "scss/10-config/mixins";    // mixins and functions
+
+// ************************************
+// Swatches
+// ************************************
+
 .sg-colors {
 	list-style: none;
 	padding-left: 0;
+}
+
+.sg-swatch--pink {
+	background-color: $joy949_pink;
+}
+
+.sg-swatch--pink-light {
+	background-color: $joy949_pink-light;
+}
+
+.sg-swatch--blue {
+	background-color: $joy949_blue;
+}
+
+.sg-swatch--green {
+	background-color: $joy949_green;
+}
+
+.sg-swatch--black {
+	background-color: $joy949_black;
+}
+
+.sg-swatch--white {
+	background-color: $joy949_white;
+}
+
+.sg-swatch--off-white {
+	background-color: $joy949_offwhite;
+}
+
+.sg-swatch--grey3 {
+	background-color: $joy949_grey3;
+}
+
+.sg-swatch--grey7 {
+	background-color: $joy949_grey7;
+}
+
+.sg-swatch--greya {
+	background-color: $joy949_greya;
 }

source/assets/css/scss/10-config/_variables.scss
@@ -1,0 +1,27 @@
+// ************************************
+// Variables
+// ************************************
+
+// ------------------------------------
+// Colours
+// ------------------------------------
+
+// pink
+$joy949_pink       : #d5007f;
+$joy949_pink-light : #f5b1db;
+
+// blue
+$joy949_blue       : #154fa0;
+
+// green
+$joy949_green      : #57a957;
+
+// shades
+$joy949_black      : #000;
+$joy949_white      : #fff;
+$joy949_offwhite   : #f1f1f1;
+
+// greys
+$joy949_grey3      : #333;
+$joy949_grey7      : #777;
+$joy949_greya      : #aaa;

commit 6ca392695d107b8a3c0d9f52d1b4937a1502acd9

gh-5 fix jshint issues
Fix jshint issues introduced in de6417030e3badfef8173f6085707ec1a3e6b55f

source/assets/js/patternlab.js
@@ -64,9 +64,9 @@
 			
 			rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
 			function hex(x) {
-				return ("0" + parseInt(x).toString(16)).slice(-2);
+				return ('0' + parseInt(x, 10).toString(16)).slice(-2);
 			}
-			return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+			return '#' + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 		}
 	}
 	